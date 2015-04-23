99e80b246f5b4a0be5c08d047108f29c1be2e284
Matt Beckett <beck24@gmail.com>
2015-04-23 01:30:32 -0700

    use owner map to limit import to admins

--- start.php
+++ start.php
@@ -9,6 +9,24 @@ const BLOG_GROUP = 559729;
 function importblogs() {
 	set_time_limit(0);
 	
+	// owner map
+	$map = array(
+		'beck24',
+		'ewinslow',
+		'brett.profitt',
+		'costelloc',
+		'juho.jaakkola',
+		'srokap',
+		'steve_clay',
+		'davetosh',
+		'bwerdmuller',
+		'marcus',
+		'pete',
+		'nickw',
+		'blacktooth'
+	);
+	
+	
 	// get our discussions
 	$options = array(
 		'type' => 'object',
@@ -24,6 +42,13 @@ function importblogs() {
 	$reply_id = get_subtype_id('object', 'discussion_reply');
 	$comment_id = get_subtype_id('object', 'comment');
 	foreach ($discussions as $d) {
+		
+		// weed out any discussions not posted by admins/former admins
+		$owner = $d->getOwnerEntity();
+		if (!in_array(strtolower($owner->username), $map)) {
+			continue;
+		}
+		
 		$d->container_guid = $d->owner_guid; // pull it out of the group
 		$d->comments_on = 'On';
 		$d->status = 'published';
